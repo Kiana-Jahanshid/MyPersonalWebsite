920c20ecb76e291f447c24b1cbed8fec241e8cc3
Kiana-Jahanshid <tannaz.maleki82@gmail.com>
2024-07-23 19:11:47 +0330

    4.7

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask , render_template , request , redirect,session as flask_session, url_for , make_response , flash
-from sqlmodel import Field , SQLModel ,Session , select , create_engine 
 from pydantic import BaseModel
 import bcrypt
-from werkzeug.datastructures import MultiDict 
-
-
+from datetime import datetime
+import json
+from database import fetch_all_users , fetch_user , fetch_comments , add_comment_to_db , add_user_to_db , relative_time_from_string
 
 class RegisterModel(BaseModel):
     username : str 
@@ -16,30 +15,21 @@ class RegisterModel(BaseModel):
     email : str
     age : int 
     confirm_password : str
+    joined_time : str
 
 class LoginModel(BaseModel):
     username : str
     password : str
-    confirm_password : str
 
-class User(SQLModel , table=True):
-    id : int = Field(default=None , primary_key=True)
-    username : str = Field(index=True)
-    password : str 
-    city : str
-    first_name : str 
-    last_name : str 
-    email : str 
-    age : int 
-    country : str 
+class CommentsModel(BaseModel):
+    username : str
+    user_id : int 
+    comment : str
+
 
 
 app = Flask(import_name="__init__" , template_folder="" , static_folder="static")
 app.secret_key = "my_secret"
-
-engine = create_engine(url="sqlite:///db/database.db" , echo=True)
-SQLModel.metadata.create_all(engine)
-
 
 
 
@@ -56,7 +46,8 @@ def contact():
 @app.route("/blog" )
 def blog(): 
     if flask_session.get("user_id"):
-        return render_template("templates/blog.html" )
+        all_comments = fetch_comments()
+        return render_template("templates/blog.html"  , all_comments=all_comments)
     else :
         flash("You have to login, to use blog page ⛔" , "info")
         return redirect(url_for("login"))
@@ -69,25 +60,19 @@ def register():
         return  render_template("templates/register.html")
     elif request.method == "POST" :
         try:
-            register_data = RegisterModel(username=request.form["username"]  , city= request.form["city"]  ,country= request.form["country"] , first_name= request.form["firstname"] , last_name= request.form["lastname"] , email= request.form["email"] , age= request.form["age"] ,password= request.form['password'], confirm_password= request.form["confirm_password"])#validating attributes type
+            register_data = RegisterModel(username=request.form["username"]  , city= request.form["city"]  ,country= request.form["country"] , first_name= request.form["firstname"] , last_name= request.form["lastname"] , email= request.form["email"] , age= request.form["age"] ,password= request.form['password'], confirm_password= request.form["confirm_password"] , joined_time=str(datetime.now()))#validating attributes type
             # print(register_data)
         except:
             flash("Type Error! One of your input was wrong" , "danger")
             return redirect(url_for("register"))
         if register_data.confirm_password == register_data.password :
-            with Session(engine) as db_session : 
-                query = select(User).where(User.username ==  request.form["username"] )
-                result = db_session.exec(query).first()
-            if not result :
+            user = fetch_user(register_data.username)
+            if not user :
                 password_byte = register_data.password.encode("utf-8")
                 hashed_password = bcrypt.hashpw(password_byte , bcrypt.gensalt())
-                with Session(engine) as db_session :
-                    new_user = User(username= request.form["username"] ,password=hashed_password , city= request.form["city"] ,country= request.form["country"] , first_name= request.form["firstname"] , last_name= request.form["lastname"] , email= request.form["email"] , age= request.form["age"] ) # create a user object
-                    db_session.add(new_user) 
-                    db_session.commit()
-                    db_session.refresh(new_user)
-                    flash("your registration compleated successfully🎉" , "success")
-                    return  redirect(url_for("login"))
+                add_user_to_db(register_data.username ,  hashed_password , register_data.city , register_data.country , register_data.first_name , register_data.last_name , register_data.email , register_data.age , register_data.joined_time)
+                flash("your registration compleated successfully🎉" , "success")
+                return  redirect(url_for("login"))
             else:
                 flash("This username is already taken ❌,Choose another one" , "danger")
                 return  redirect(url_for("register"))
@@ -103,32 +88,25 @@ def login():
         return  render_template("templates/login.html")
     elif request.method == "POST" : 
         try :            
-            register_login_data = LoginModel(username= request.form["username"] , password= request.form["password"] , confirm_password= request.form["confirm_password"])# if email & pass types are correct , user will be navigated to upload page
+            register_login_data = LoginModel(username= request.form["username"] , password= request.form["password"])# if email & pass types are correct , user will be navigated to upload page
         except:
             flash("type error")
             return  redirect(url_for("login"))
-        with Session(engine) as db_session :
-            query = select(User).where(User.username == register_login_data.username ) #    User.password == register_login_data.password)
-            user = db_session.exec(query).first()
-        if  request.form["confirm_password"] ==  request.form["password"] :
-            
-            if user :
-                byte_password = register_login_data.password.encode("utf-8")
-                if bcrypt.checkpw(byte_password , user.password):  
-                    flask_session["username"]  =  register_login_data.username
-                    flash("You logged in successfully 🎉" , "success")
-                    flask_session["user_id"] = user.id
-                    return redirect(url_for("root")) 
-                else:
-                    flash("Password is INCORRECT" , "danger")
-                    return redirect(url_for("login"))
-            else :
-                flash("Username is INCORRECT" , "danger")
+        user = fetch_user(register_login_data.username)
+        if user :
+            byte_password = register_login_data.password.encode("utf-8")
+            if bcrypt.checkpw(byte_password , user.password ):  
+                flask_session["username"]  =  register_login_data.username
+                flask_session["user_id"] = user.id
+                flash("You logged in successfully 🎉" , "success")
+                return redirect(url_for("root")) 
+            else:
+                flash("Password is INCORRECT ❌" , "danger")
                 return redirect(url_for("login"))
         else :
-            flash("Confirm-password doesn't match with password ,Try again ..." , "warning")
+            flash("Username is INCORRECT ❌" , "danger")
             return redirect(url_for("login"))
-
+            
 
 @app.route("/logout")
 def logout():
@@ -137,8 +115,53 @@ def logout():
         flash("You logged out successfully" , "success")
         return redirect(url_for("root"))
     else:
-        return redirect(url_for("root"))
+        flash("You are not not logged in yet ! " , "warning")
+        render_template("templates/index.html")
+
+
+@app.route("/admin" , methods=["GET" , "POST"])
+def pannel_admin():
+    if flask_session.get("user_id"):
+        users , user_count = fetch_all_users()
+        for user in users :
+            joined_time = str(user.joined_time)
+            parsed_time = datetime.strptime(joined_time, '%Y-%m-%d %H:%M:%S.%f')
+            formatted_time = parsed_time.strftime('%Y-%m-%d %H:%M:%S')
+            user.joined_time = relative_time_from_string(formatted_time)  
+        comments = fetch_comments()
+        return  render_template("admin.html" , username= flask_session.get("username") , users=users , user_count=user_count , comments=comments)
+    else :
+        flash("first login to website to access admin pannel ⛔")
+        return redirect(url_for("login"))
+
+
+@app.route("/add_comment" , methods=["POST"])
+def comments():
+    if flask_session.get("user_id") :
+        comment = request.form["text"]
+        comment_model = CommentsModel(username=flask_session.get("username") , user_id=flask_session.get("user_id") , comment=comment)
+        comment = comment[3:-4]
+        add_comment_to_db(comment_model.user_id , comment_model.username , comment)
+        return redirect(url_for("blog"))
+    else :
+        flash("first you have to login ⛔" , "danger")
+        return redirect(url_for("login"))
+
+
+@app.route("/api" , methods=["GET" ,"POST"])
+def send_personal_info():
+    info = {"Fisrtname" : "kiana",
+            "lastname"  : "jahanshid",
+            "job" : "future programmer", 
+            "age" : "30" , 
+            "degree" : "master of science"}
+    jsonformat = json.dumps(info , indent=1)
+    jsonformat = json.loads(jsonformat)
+    return render_template("templates/info.html" , json=jsonformat)
+
+
 
 
 # HOW TO RUN 
 # flask --app app run --debug
+
---
+++ database.py
@@ -0,0 +1,84 @@
+
+from sqlmodel import Field , SQLModel ,Session , select , create_engine
+from datetime import datetime
+
+class User(SQLModel , table=True):
+    __tablename__ = "users"
+    id : int = Field(default=None , primary_key=True)
+    username : str = Field(index=True)
+    password : str 
+    city : str
+    first_name : str 
+    last_name : str 
+    email : str 
+    age : int 
+    country : str 
+    joined_time : str
+
+class Comment(SQLModel , table=True):
+    __tablename__ = "comments"
+    id : int = Field(default=None , primary_key=True) 
+    username : str
+    user_id : int = Field(foreign_key="users.id")
+    text : str
+    time_stamp : datetime = Field(default_factory=datetime.now)
+
+
+
+
+engine = create_engine(url="sqlite:///db/database.db" , echo=True)
+SQLModel.metadata.create_all(engine)
+
+
+def fetch_user(username):
+    with Session(engine) as db_session:
+        user = select(User).where(User.username == username)
+        user = db_session.exec(user).first()
+        return user 
+    
+
+def fetch_all_users():
+    with Session(engine) as db_session :
+        users = select(User)
+        users = list(db_session.exec(users))
+        users_count = users.count()
+        return users , users_count
+
+def fetch_comments():
+    with Session(engine) as db_session :
+        comments_and_usernames = select(Comment.username , Comment.text)
+        comments_and_usernames = list(db_session.exec(comments_and_usernames))
+        return comments_and_usernames
+    
+def add_user_to_db(username ,  hashed_password ,city , country ,first_name , last_name , email , age , joined_time):
+    with Session(engine) as db_session:
+        new_user = User(username=username ,password=hashed_password  , city=city, country=country , first_name= first_name , last_name=last_name , email=email , age=age  ,joined_time=joined_time )
+        db_session.add(new_user)
+        db_session.commit()
+        db_session.refresh(new_user)
+
+
+def add_comment_to_db( user_id , comment , username ):
+    with Session(engine) as db_session:
+        new_comment = Comment(user_id=user_id ,username=username, text=comment)
+        db_session.add(new_comment)
+        db_session.commit()
+        db_session.refresh(new_comment)
+
+
+def relative_time_from_string(time_string):
+    parsed_time = datetime.strptime(time_string, '%Y-%m-%d %H:%M:%S')
+    current_time = datetime.now()
+    time_difference = current_time - parsed_time
+    seconds = time_difference.total_seconds()
+    if seconds < 60:
+        return f"{int(seconds)} seconds ago"
+    elif seconds < 3600:
+        minutes = int(seconds // 60)
+        return f"{minutes} minutes ago"
+    elif seconds < 86400:
+        hours = int(seconds // 3600)
+        return f"{hours} hours ago"
+    else:
+        days = int(seconds // 86400)
+        return f"{days} days ago"
